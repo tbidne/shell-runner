a3b381c4beb1646600e440323785f4c537e2476a
Tommy Bidne <tbidne@protonmail.com>
2023-03-08 17:23:54 +1300

    Add caches to flake

--- flake.nix
+++ flake.nix
@@ -49,9 +49,28 @@
       flake = pkgs.shrunProject.flake {
         # This adds support for `nix build .#js-unknown-ghcjs:hello:exe:hello`
         # crossPlatforms = p: [p.ghcjs];
+        #crossPlatforms = p: p.musl64;
       };
     in
     flake // {
       packages.default = flake.packages."shrun:exe:shrun";
     });
+  nixConfig = {
+    extra-substituters = [
+      "https://nixcache.reflex-frp.org"
+      "https://cache.nixos.org"
+      "https://cache.iog.io"
+      "https://cache.zw3rk.com"
+    ];
+
+    extra-trusted-public-keys = [
+      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+    ];
+
+    # Adding this even though it doesn't seem to work...
+    allow-import-from-derivation = "true";
+  };
 }
